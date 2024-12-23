311859ef361409fbce965a586295411626aae5ae
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-23 14:19:28 -0300

    Add files via upload

---
+++ Quadrado de Asteriscos.py
@@ -0,0 +1,7 @@
+# Solicita o tamanho do lado do quadrado
+tamanho = int(input("Digite o tamanho do lado do quadrado: "))
+
+# Exibe o quadrado de asteriscos
+for _ in range(tamanho):
+    print("*" * tamanho)
+input()
